59936dc7cd570c7437aa0aa12145cc446de9f48d
Elton Okawa <elton.okawa@gmail.com>
2022-05-10 08:07:41 -0300

    draft

--- client/scripts/index.ts
+++ client/scripts/index.ts
@@ -1,13 +1,27 @@
+import Colyseus from "../typing/colyseus";
+import { GameRoomState } from "../typing/GameRoomState";
+import PIXI from "../typing/pixi";
+
 const client = new Colyseus.Client('ws://localhost:2567');
 
+let currentState: any = {};
+
 function connect() {
-  client.joinOrCreate('game_room').then(room => {
+  client.joinOrCreate<GameRoomState>('game_room').then(room => {
     console.log(room.sessionId, 'joined', room.name);
 
     const app = new PIXI.Application({ width: 640, height: 360 });
     document.body.appendChild(app.view);
+    const sprite = PIXI.Sprite.from('./static/happy-face.png');
+    app.stage.addChild(sprite);
+    app.ticker.add((delta) => {
+      const body: any = Object.values(currentState?.bodies ?? {})?.[0] ?? {};
+      sprite.x = body?.position?.x ?? 100;
+      sprite.y = body?.position?.y ?? 100;
+    });
 
     room.onStateChange(state => {
+      currentState = state;
       // console.log(state);
     });
   }).catch(e => {
---
+++ client/typing/VectorSchema.ts
@@ -0,0 +1,14 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 1.0.34
+// 
+
+import { Schema, type, ArraySchema, MapSchema, SetSchema, DataChange } from '@colyseus/schema';
+
+
+export class VectorSchema extends Schema {
+    @type("number") public x!: number;
+    @type("number") public y!: number;
+}
