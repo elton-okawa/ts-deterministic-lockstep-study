4f021ebbd3217f09376dce698d1a848ba9a75607
Elton Okawa <elton.okawa@gmail.com>
2022-05-17 21:03:57 -0300

    feat: show ping

---
+++ client/scripts/Ping.ts
@@ -0,0 +1,35 @@
+const WINDOW_SIZE = 10;
+const PING_INTERVAL_MS = 500;
+
+export class Ping {
+  private window: number[];
+  private counter: number;
+  private time: number;
+  private pinger: () => void;
+
+  constructor(pinger: () => void) {
+    this.window = Array.from({ length: WINDOW_SIZE }, () => 0);
+    this.counter = 0;
+    this.pinger = pinger;
+  }
+
+  get ping() {
+    const total = this.window.reduce((total, current) => total += current, 0);
+    return Math.round(total / WINDOW_SIZE);
+  }
+
+  set ping(arg: number) {
+    this.window[this.counter] = arg;
+    this.counter = (this.counter + 1) % WINDOW_SIZE;
+  }
+
+  performPing() {
+    this.time = Date.now();
+    this.pinger();
+  }
+
+  handlePong() {
+    this.ping = Date.now() - this.time;
+    setTimeout(this.performPing.bind(this), PING_INTERVAL_MS);
+  }
+}
