ed7e58e9f36b60d2e67d01b8be703057e36bd1e3
Elton Okawa <elton.okawa@gmail.com>
2022-05-22 08:08:37 -0300

    feat: return a list of forced confirmations

--- src/helpers/InputFrameManager.ts
+++ src/helpers/InputFrameManager.ts
@@ -1,18 +1,21 @@
+export interface ForceConfirmation {
+  id: string;
+  lastConfirmedFrame: number;
+}
+
 export class InputFrameManager {
 
   private _startFrame: number;
   private _rollbackWindow: number;
   private _lastConfirmedFrame: number;
-  private _lastPlayerFrame: number[];
-  private _idToIndex: { [key: string]: number };
+  private _lastPlayerFrame: { [key: string]: number };
 
   constructor(startFrame: number, rollbackWindow: number) {
     this._startFrame = startFrame;
     this._rollbackWindow = rollbackWindow;
 
     this._lastConfirmedFrame = startFrame;
-    this._lastPlayerFrame = [];
-    this._idToIndex = {};
+    this._lastPlayerFrame = {};
   }
 
   get confirmedFrame() {
@@ -21,37 +24,37 @@ export class InputFrameManager {
 
   addPlayer(id: string) {
     // first client input will be raw frame 1 resulting in 1 + STATIC_DELAY
-    this._lastPlayerFrame.push(this._startFrame);
-    this._idToIndex[id] = this._lastPlayerFrame.length - 1;
+    this._lastPlayerFrame[id] = this._startFrame;
   }
 
   removePlayer(id: string) {
-    const playerIndex = this._idToIndex[id];
-    this._lastPlayerFrame.splice(playerIndex, 1);
-    delete this._idToIndex[id];
+    delete this._lastPlayerFrame[id];
   }
 
   confirmInput(id: string, inputFrame: number) {
     const targetFrame = inputFrame + this._startFrame;
-    const playerIndex = this._idToIndex[id];
-    if (this._lastPlayerFrame[playerIndex] < targetFrame) {
-      this._lastPlayerFrame[playerIndex] = targetFrame;
+    if (this._lastPlayerFrame[id] < targetFrame) {
+      this._lastPlayerFrame[id] = targetFrame;
       this.tryToConfirmFrame(targetFrame);
     } else {
-      console.error(`Cannot confirm same or older input (current: ${this._lastPlayerFrame[playerIndex]}, received: ${targetFrame})`);
+      console.error(`Cannot confirm same or older input (current: ${this._lastPlayerFrame[id]}, received: ${targetFrame})`);
     }
   }
 
-  tryToForceConfirmation(currentFrame: number): boolean {
-    let forced = false;
+  tryToForceConfirmation(currentFrame: number): null | ForceConfirmation[] {
+    let forced: ForceConfirmation[] = null;
     const minConfirmedFrame = currentFrame - this._rollbackWindow;
     if (this._lastConfirmedFrame < minConfirmedFrame) {
       this._lastConfirmedFrame = minConfirmedFrame;
+      forced = [];
 
-      for (let i = 0; i < this._lastPlayerFrame.length; i++) {
-        if (this._lastPlayerFrame[i] < minConfirmedFrame) {
-          this._lastPlayerFrame[i] = minConfirmedFrame;
-          forced = true;
+      for (const id of Object.keys(this._lastPlayerFrame)) {
+        if (this._lastPlayerFrame[id] < minConfirmedFrame) {
+          forced.push({
+            id: id,
+            lastConfirmedFrame: this._lastPlayerFrame[id],
+          });
+          this._lastPlayerFrame[id] = minConfirmedFrame;
         }
       }
     }
@@ -60,7 +63,7 @@ export class InputFrameManager {
   }
 
   private tryToConfirmFrame(inputFrame: number) {
-    const shouldConfirm = this._lastPlayerFrame
+    const shouldConfirm = Object.values(this._lastPlayerFrame)
       .map((frame) => frame > this._lastConfirmedFrame)
       .reduce((prev, curr) => prev && curr, true);
 
