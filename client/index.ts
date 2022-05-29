a359fc7af471ddb81a631023994ec74bfc9180b8
Elton Okawa <elton.okawa@gmail.com>
2022-05-29 18:35:03 -0300

    fix: state(t+1) = state(t) + input(t)

--- client/index.ts
+++ client/index.ts
@@ -267,7 +267,8 @@ function simulatePhysicsFrame(frame: number) {
     world.applyInput(player.id, input);
   });
 
-  world.update(frame);
+  // state(t+1) = state(t) + input(t)
+  world.update(frame + 1);
 }
 
 setupHtml();
