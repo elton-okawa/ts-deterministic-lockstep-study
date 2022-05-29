251b7a91e0cffdd37e6bc1f4bbc3e6e921d55b29
Elton Okawa <elton.okawa@gmail.com>
2022-05-29 10:36:28 -0300

    feat: add lodash

--- client/typing/globals.d.ts
+++ client/typing/globals.d.ts
@@ -1,9 +1,11 @@
 import * as Colyseus from './colyseus';
 import * as PIXI from './pixi';
 import * as RAPIER from './rapierjs';
+import lodash from 'lodash';
 
 declare global {
   const Colyseus: typeof Colyseus;
   const PIXI: typeof PIXI;
   const RapierType: typeof RAPIER;
+  const _: typeof lodash;
 }
